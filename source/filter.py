541aecdd2551f7262b8052822b3722f919a47f19
Limon <valerijf@stud.ntnu.no>
2016-01-29 11:25:45 +0100

    Added gate pos tagger with basic filtering

---
+++ source/filter.py
@@ -0,0 +1,63 @@
+import filters as f
+from subprocess import Popen, PIPE, STDOUT
+import io
+import re
+import os
+import time
+
+pos_tags_RE = re.compile(ur'_([A-Z]+)\s')
+gate_tagger_path = "gate_pos_tagger/"
+
+
+def filter_tweets(tweets):
+    filt = [f.no_username, f.no_url, f.no_emoticons, f.no_hash]
+    filtered = []
+
+    for tweet in tweets:
+        for fil in filt:
+            tweet = fil(tweet)
+        filtered.append(tweet)
+    return filtered
+
+
+def run_tagger(raw_tweets):
+    temp_file = "filtered.temp"
+    f = io.open(gate_tagger_path + temp_file, "w", encoding="utf-8")
+    f.write("\n".join(raw_tweets))
+    f.close()
+
+    old_dir = os.getcwd()
+    os.chdir(gate_tagger_path)
+    p = Popen(['java', '-Xmx1024m', '-jar', 'twitie_tag.jar', 'models/gate-EN-twitter-fast.model', temp_file],
+              stdout=PIPE, stderr=STDOUT)
+    os.chdir(old_dir)
+
+    raw_pos = [' '.join(line.split()) for line in p.stdout if "_" in line]
+    os.remove(gate_tagger_path + temp_file)
+    return raw_pos
+
+
+def main():
+    start_time = time.time()
+
+    print "Reading...",
+    tweets = io.open(gate_tagger_path + "2m.txt", "r", encoding="utf-8").read().split("\n")[:50000]
+    print "done"
+
+    print "Filtering...",
+    tweets = filter_tweets(tweets)
+    print "done"
+
+    print "Tagging...",
+    tweets = run_tagger(tweets)
+    print "done"
+
+    print "Writing...",
+    out = open(gate_tagger_path + "tagged.txt", "w")
+    out.write('\n'.join(tweets))
+    out.close()
+    print "done"
+
+    print "Finished in", "%.2f" % (time.time()-start_time), "sec"
+
+main()
---
+++ source/filters.py
@@ -0,0 +1,100 @@
+"""
+    A collection of different filter methods for the tweets.
+"""
+import HTMLParser
+import re
+import tokenizer
+
+# Emoticon definitions.
+NormalEyes = r'[:=8]'
+HappyEyes = r'[xX]'
+WinkEyes = r'[;]'
+NoseArea = r'[\-o\Oc\^\*\']?'
+HappyMouths = r'[dD\)\]\*\>\}]'
+SadMouths = r'[c<|@L\(\[\/\{\\]'
+TongueMouths = r'[pP]'
+
+Positive_RE = re.compile('(\^_\^|' + "((" + NormalEyes + "|" + HappyEyes + "|" + WinkEyes + ")" + NoseArea + HappyMouths + ')|(?:\<3+))', re.UNICODE)
+Negative_RE = re.compile(NormalEyes + NoseArea + SadMouths, re.UNICODE)
+
+Wink_RE = re.compile(WinkEyes + NoseArea + HappyMouths, re.UNICODE)
+Tongue_RE = re.compile(NormalEyes + NoseArea + TongueMouths, re.UNICODE)
+
+Emoticon = (
+    "(" + NormalEyes + "|" + HappyEyes + "|" + WinkEyes + ")" + NoseArea +
+    "(" + TongueMouths + "|" + SadMouths + "|" + HappyMouths + ")"
+)
+Emoticon_RE = re.compile(Emoticon, re.UNICODE)
+
+# Tag definitions
+username_RE = re.compile(r'(@[a-zA-Z0-9_]{1,15})')
+hashtag_RE = re.compile(r'(#[a-zA-Z]+[a-zA-Z0-9_]*)')
+rt_tag_RE = re.compile(r'(^RT\s+|\s+RT\s+)')
+quote_RE = re.compile(r'".*?"')
+url_RE = re.compile(r'(\w+:\/\/\S+)')
+characters_RE = re.compile(r"[^a-zA-Z !?,.:()']")
+characters_limit_RE = re.compile(r"[^a-zA-Z ]")
+
+punctuation = {',', '.', ':', ';', '!', '?'}
+word_finder = re.compile(r'(\S+)')
+
+
+def html_decode(tweet_text):
+    h = HTMLParser.HTMLParser()
+    return h.unescape(tweet_text)
+
+def tokenize(tweet):
+    return ' '.join(tokenizer.tokenize(tweet))
+
+def limit_chars(tweet):
+    return characters_limit_RE.sub("", tweet)
+
+def lower_case(tweet):
+    return tweet.lower()
+
+
+def no_emoticons(tweet_text):
+    tweet = re.sub(Positive_RE, "", tweet_text)
+    tweet = re.sub(Negative_RE, "", tweet)
+    tweet = re.sub(Emoticon_RE, "", tweet)
+    return tweet
+
+
+def no_username(tweet_text):
+    return username_RE.sub("", tweet_text)
+
+def username_placeholder(tweet_text):
+    return username_RE.sub("||U||", tweet_text)
+
+
+def no_hash(tweet_text):
+    return hashtag_RE.sub("", tweet_text)
+
+def hash_placeholder(tweet_text):
+    return hashtag_RE.sub("||H||", tweet_text)
+
+
+def no_rt_tag(tweet_text):
+    return rt_tag_RE.sub("", tweet_text)
+
+
+def no_url(tweet_text):
+    return url_RE.sub("", tweet_text)
+
+def url_placeholder(tweet_text):
+    return url_RE.sub("||URL||", tweet_text)
+
+
+def no_quotations(tweet_text):
+    return quote_RE.sub("", tweet_text)
+
+def quote_placeholder(tweet_text):
+    return quote_RE.sub("||QUOTE||", tweet_text)
+
+
+def reduce_letter_duplicates(tweet_text):
+    return re.sub(r'(.)\1{3,}', r'\1\1\1', tweet_text, flags=re.IGNORECASE)
+
+
+def hash_as_normal(tweet_text):
+    return re.sub(r'#([a-zA-Z]+[a-zA-Z0-9_]*)', "\\1", tweet_text)
