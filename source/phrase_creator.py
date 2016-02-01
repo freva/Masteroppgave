b92e519a5b59c57deb59bc8113e42ec9277b5851
valerijf <valerijf@stud.ntnu.no>
2016-02-01 15:11:37 +0100

    Started simplifying

--- source/phrase_creator.py
+++ source/phrase_creator.py
@@ -1,9 +1,6 @@
+from collections import defaultdict
 import re
-import operator
-import io
 
-start_tags = ['_JJ', '_RB', '_NN']
-phrases = {}
 
 descriptive_phrases = {
     '_JJ': [['_NN'], ['_JJ', '_NN']],
@@ -11,49 +8,49 @@ descriptive_phrases = {
     '_NN': [['_JJ', '_NN']]
 }
 
-regex = re.compile('[^a-zA-Z]')
+phrases = defaultdict(list)
+pos_tag = re.compile(r'_[A-Z]+\b')
+start_tags = re.compile(r'\w+_(JJ|NN|RB]).?$')
+
 
 
 def main():
-    file = open('gate_pos_tagger/tagged.txt', 'r').read().split("\n")
-    for j in range(len(file)):
-        list = file[j].split(" ")
-        for i in range(len(list)):
-            for tag in start_tags:
-                if list[i].find(tag) > 0 and len(list)-i > 1:
-                    check_consecutive_words(list, i, tag, j)
+    tweets = open('gate_pos_tagger/tagged.txt', 'r').read().split("\n")
+    for tweetID, tweet in enumerate(tweets):
+        words = tweet.split()
+        for wordIndex in range(len(words)-1):
+            tag = start_tags.findall(words[wordIndex])
+            if tag:
+                check_consecutive_words(words[wordIndex: wordIndex+2], "_" + tag[0], tweetID)
 
-    sorted_phrases = sorted(phrases.items(), key=operator.itemgetter(1), reverse=True)
-    for sorted_phrase in sorted_phrases:
-        if len(sorted_phrase[1]) >= 50:
-            print sorted_phrase[0]
+    filtered_phrases = {key: len(value) for key, value in phrases.items() if len(value) >= 100}
+    sorted_phrases = sorted(filtered_phrases.items(), key=lambda k: k[1], reverse=True)
+
+    for phrase, freq in sorted_phrases:
+        print phrase.ljust(30), freq
 
 
-def check_consecutive_words(sentence, index, tag, tweet_nr):
+def check_consecutive_words(sentence, tag, tweet_nr):
     possible_tags = descriptive_phrases.get(tag)
-    if len(sentence) - index >= 3:
-        for i in range(len(possible_tags)):
-            if sentence[index+1].find(possible_tags[i][0]) > 0:
-                if len(possible_tags[i]) > 1:
-                    if sentence[index+2].find(possible_tags[i][1]) > 0:
+    if len(sentence) == 3:
+        for possible_tag in possible_tags:
+            if sentence[1].find(possible_tag[0]) > 0:
+                if len(possible_tag) > 1:
+                    if sentence[2].find(possible_tag[1]) > 0:
                         continue
-                create_and_add_phrase(sentence, index, tweet_nr)
+                create_and_add_phrase(sentence, tweet_nr)
     else:
-        for i in range(len(possible_tags)):
-            if sentence[index+1].find(possible_tags[i][0]) > 0:
-                create_and_add_phrase(sentence, index, tweet_nr)
+        for possible_tag in possible_tags:
+            if sentence[1].find(possible_tag[0]) > 0:
+                create_and_add_phrase(sentence, tweet_nr)
                 break
 
 
-def create_and_add_phrase(sentence, index, tweet_nr):
-    phrase = regex.sub('',sentence[index].split('_')[0]) + " " + regex.sub('', sentence[index+1].split('_')[0])
+def create_and_add_phrase(sentence, tweet_nr):
+    phrase = pos_tag.sub('', ' '.join(sentence[:2]))
     phrase = re.sub('[!?.]', '', phrase)
     phrase = phrase.lower()
-    if phrase in phrases:
-        phrases[phrase].append(tweet_nr)
-    else:
-        phrases[phrase] = []
-        phrases[phrase].append(tweet_nr)
+    phrases[phrase].append(tweet_nr)
 
 
 if __name__ == '__main__':
