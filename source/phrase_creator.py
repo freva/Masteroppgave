1107c6191ca18f55193573d6a7b89b13421aa9d5
valerijf <valerijf@stud.ntnu.no>
2016-02-01 15:39:32 +0100

    Simplified

--- source/phrase_creator.py
+++ source/phrase_creator.py
@@ -3,15 +3,15 @@ import re
 
 
 descriptive_phrases = {
-    '_JJ': [['_NN'], ['_JJ', '_NN']],
-    '_RB': [['_JJ', '_NN'], ['_VB']],
-    '_NN': [['_JJ', '_NN']]
+    'JJ': [['_NN'], ['_JJ', '_NN']],
+    'RB': [['_JJ', '_NN'], ['_VB']],
+    'NN': [['_JJ', '_NN']]
 }
 
 phrases = defaultdict(list)
 pos_tag = re.compile(r'_[A-Z]+\b')
-start_tags = re.compile(r'\w+_(JJ|NN|RB]).?$')
-
+start_tags = re.compile(r'\S+_(JJ|NN|RB)')
+non_alphabetic = re.compile(r'[^a-z ]')
 
 
 def main():
@@ -21,7 +21,7 @@ def main():
         for wordIndex in range(len(words)-1):
             tag = start_tags.findall(words[wordIndex])
             if tag:
-                check_consecutive_words(words[wordIndex: wordIndex+2], "_" + tag[0], tweetID)
+                check_consecutive_words(words[wordIndex: wordIndex+2], tag[0], tweetID)
 
     filtered_phrases = {key: len(value) for key, value in phrases.items() if len(value) >= 100}
     sorted_phrases = sorted(filtered_phrases.items(), key=lambda k: k[1], reverse=True)
@@ -31,7 +31,7 @@ def main():
 
 
 def check_consecutive_words(sentence, tag, tweet_nr):
-    possible_tags = descriptive_phrases.get(tag)
+    possible_tags = descriptive_phrases[tag]
     if len(sentence) == 3:
         for possible_tag in possible_tags:
             if sentence[1].find(possible_tag[0]) > 0:
@@ -48,8 +48,9 @@ def check_consecutive_words(sentence, tag, tweet_nr):
 
 def create_and_add_phrase(sentence, tweet_nr):
     phrase = pos_tag.sub('', ' '.join(sentence[:2]))
-    phrase = re.sub('[!?.]', '', phrase)
     phrase = phrase.lower()
+    phrase = non_alphabetic.sub('', phrase)
+
     phrases[phrase].append(tweet_nr)
 
 
