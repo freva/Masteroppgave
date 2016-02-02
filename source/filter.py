6ddc86bfc12fc353a41923835fd18b8b286f1ba6
Limon <valerijf@stud.ntnu.no>
2016-02-02 18:24:45 +0100

    Removed batching

--- source/filter.py
+++ source/filter.py
@@ -1,5 +1,5 @@
 import filters as f
-from subprocess import Popen, PIPE, STDOUT
+from unicodefixer import fix_bad_unicode
 import io
 import re
 import os
@@ -9,49 +9,30 @@ pos_tags_RE = re.compile(ur'_([A-Z]+)\s')
 gate_tagger_path = "gate_pos_tagger/"
 
 
-def filter_tweets(filename):
+def filter_tweets(input_name, output_name):
     filt = [f.no_username, f.no_url, f.no_emoticons, f.no_hash, f.regular_text_only]
-    interim_list = []
-    batch_size = 50000
 
-    with io.open(filename, "r", encoding="utf-8") as tweet_file:
-        for tweet in tweet_file:
+    with io.open(input_name, "r", encoding="utf-8") as in_file, open(output_name, "w") as out_file:
+        for tweet in in_file:
             for fil in filt:
                 tweet = fil(tweet)
-            interim_list.append(tweet)
-            if len(interim_list) >= batch_size:
-                yield interim_list
-                interim_list = []
-        if interim_list:
-            yield interim_list
+            out_file.write(tweet + "\n")
 
 
-def run_tagger(filename):
-    temp_file = "filtered.temp"
-
-    for filtered_tweets in filter_tweets(filename):
-        f = io.open(gate_tagger_path + temp_file, "w", encoding="utf-8")
-        f.write("\n".join(filtered_tweets))
-        f.close()
-
-        old_dir = os.getcwd()
-        os.chdir(gate_tagger_path)
-        p = Popen(['java', '-Xmx1024m', '-jar', 'twitie_tag.jar', 'models/gate-EN-twitter-fast.model', temp_file],
-                  stdout=PIPE, stderr=STDOUT)
-        os.chdir(old_dir)
-
-        yield [' '.join(line.split()) for line in p.stdout if "_" in line]
-    os.remove(gate_tagger_path + temp_file)
+def run_tagger(input_name, output_name):
+    old_dir = os.getcwd()
+    os.chdir(gate_tagger_path)
+    os.system('java -Xmx1024m -jar twitie_tag.jar models/gate-EN-twitter-fast.model ' +
+              input_name + ' > ' + output_name)
+    os.chdir(old_dir)
 
 
 def main():
     start_time = time.time()
 
-    with open(gate_tagger_path + "tagged.txt", "w") as out:
-        for tagged_tweets in run_tagger(gate_tagger_path + "2m.txt"):
-            out.write('\n'.join(tagged_tweets) + '\n')
+    filter_tweets(gate_tagger_path + "200k.txt", gate_tagger_path + "filtered.txt")
+    run_tagger("filtered.txt", "tagged.txt")
 
     print "Finished in", "%.2f" % (time.time()-start_time), "sec"
 
 main()
-
