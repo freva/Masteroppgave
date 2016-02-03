e8c85fa6d7aad9459bd36defb55ddb9de6e725cf
Brage <brageej@stud.ntnu.no>
2016-02-03 17:42:24 +0100

    Basic implementation of graph creation. Propagation not dealt with yet.

---
+++ src/com/freva/masteroppgave/graph/Edge.java
@@ -0,0 +1,19 @@
+
+public class Edge {
+
+    private Node neighbor;
+    private double weight;
+
+    public Edge(Node neighbor, double weight) {
+        this.neighbor = neighbor;
+        this.weight = weight;
+    }
+
+    public Node getNeighbor() {
+        return neighbor;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+}
