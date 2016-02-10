7a2b94147b9be043b1ea498e9b98b6479724fa2a
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-02-10 23:17:26 +0100

    Rewritten JSONLineByLine to use GSON

--- src/com/freva/masteroppgave/utils/JSONLineByLine.java
+++ src/com/freva/masteroppgave/utils/JSONLineByLine.java
@@ -1,30 +1,39 @@
 package com.freva.masteroppgave.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.lang.reflect.Type;
+import java.util.*;
 
-public class JSONLineByLine<K, V> {
+public class JSONLineByLine<T> {
     private Scanner scanner;
+    private Type type;
 
-    public JSONLineByLine(String filename) throws FileNotFoundException {
-        scanner = new Scanner(new File(filename));
+    /**
+     * Class to read JSON strings line by line, where each line contains a <T> entry.
+     * @param filename File path to line separated JSON file
+     * @throws FileNotFoundException
+     */
+    public JSONLineByLine(String filename, Type type) throws FileNotFoundException {
+        this.scanner = new Scanner(new File(filename));
+        this.type = type;
     }
 
 
+    /**
+     * Check if there are more entries left
+     * @return True if more entries present
+     */
     public boolean hasNext() {
         return scanner.hasNext();
     }
 
-    public Map.Entry<K, V> next() throws JSONException {
-        JSONObject json = new JSONObject(scanner.nextLine());
-        Object key = json.keys().next();
-        V value = (V) json.get(String.valueOf(key));
-        return new AbstractMap.SimpleEntry<K, V>((K) key, value);
+
+    public T next() throws JSONException {
+        return new Gson().fromJson(scanner.nextLine(), type);
     }
 }
