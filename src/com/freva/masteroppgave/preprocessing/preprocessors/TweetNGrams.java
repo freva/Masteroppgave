f075ee91ac887381a74513c16dfce3ae4a0b5c82
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-02-14 18:33:37 +0100

    TweetNGrams delegates reading and preprocessing tweets to TweetReader

--- src/com/freva/masteroppgave/preprocessing/preprocessors/TweetNGrams.java
+++ src/com/freva/masteroppgave/preprocessing/preprocessors/TweetNGrams.java
@@ -1,52 +1,56 @@
 package com.freva.masteroppgave.preprocessing.preprocessors;
 
-import com.freva.masteroppgave.preprocessing.filters.Filters;
 import com.freva.masteroppgave.preprocessing.utils.NGrams;
-import com.freva.masteroppgave.utils.FileUtils;
 import com.freva.masteroppgave.utils.progressbar.Progressable;
 
 import java.io.*;
 import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 
 public class TweetNGrams implements Progressable {
-    private int totalLines = 0;
-    private int lineCounter = 0;
+    private TweetReader tweetReader;
+    private String output_filename;
+
+    /**
+     *
+     * @param input_filename File path to tweets to generate n-grams for
+     * @param output_filename File path to write n-grams to
+     * @param filters Set of filters to apply before generating n-grams
+     * @throws IOException
+     */
+    @SafeVarargs
+    public TweetNGrams(String input_filename, String output_filename, Function<String, String>... filters) throws IOException {
+        this.output_filename = output_filename;
+        this.tweetReader = new TweetReader(input_filename, filters);
+    }
+
 
     /**
      * Writes all frequent n-grams found in a file to another file in JSON format
-     * @param input_filename File path to tweets to generate n-grams for
-     * @param output_filename File path to write n-grams to
      * @param frequencyCutoff Percentage of total number of tweets that n-gram must have appeared in to be included
      * @throws IOException
      */
-    public void createNGrams(String input_filename, String output_filename, double frequencyCutoff) throws IOException {
-        this.totalLines = FileUtils.countLines(input_filename);
+    public void createFrequentNGrams(double frequencyCutoff) throws IOException {
         Map<String, List<Integer>> nGramsCounter = new HashMap<>();
         Pattern containsAlphabet = Pattern.compile(".*[a-zA-Z]+.*");
+        int lineCounter;
 
-        try(BufferedReader br = new BufferedReader(new FileReader(input_filename))) {
-            for(String line; (line = br.readLine()) != null; lineCounter++) {
-                if(lineCounter % 50000 == 0 && lineCounter != 0) {
-                    removeEntriesUnderThreshold(nGramsCounter, (int) (frequencyCutoff * lineCounter) / 2);
+        for(lineCounter=0; tweetReader.hasNext(); lineCounter++) {
+            if(lineCounter % 50000 == 0 && lineCounter != 0) {
+                removeEntriesUnderThreshold(nGramsCounter, (int) (frequencyCutoff * lineCounter) / 2);
+            }
+
+            String line = tweetReader.readAndPreprocessNextTweet();
+            for(String nGram: NGrams.getSyntacticalNGrams(line, 6)) {
+                if(! containsAlphabet.matcher(nGram).find()) continue;
+
+                if(! nGramsCounter.containsKey(nGram)) {
+                    nGramsCounter.put(nGram, new ArrayList<>());
                 }
 
-                line = Filters.chain(line,
-                        Filters::HTMLUnescape, Filters::removeUnicodeEmoticons, Filters::normalizeForm,
-                        Filters::removeURL, Filters::removeRTTag, Filters::removeHashtag, Filters::removeUsername,
-                        Filters::removeEmoticons, Filters::removeInnerWordCharacters, Filters::removeNonAlphanumericalText,
-                        Filters::removeFreeDigits, Filters::removeRepeatedWhitespace, String::trim, String::toLowerCase);
-
-                for(String nGram: NGrams.getSyntacticalNGrams(line, 6)) {
-                    if(! containsAlphabet.matcher(nGram).find()) continue;
-
-                    if(! nGramsCounter.containsKey(nGram)) {
-                        nGramsCounter.put(nGram, new ArrayList<>());
-                    }
-
-                    nGramsCounter.get(nGram).add(lineCounter);
-                }
+                nGramsCounter.get(nGram).add(lineCounter);
             }
         }
 
@@ -72,6 +76,6 @@ public class TweetNGrams implements Progressable {
 
     @Override
     public double getProgress() {
-        return (totalLines == 0 ? 0 : 100.0*lineCounter/totalLines);
+        return tweetReader.getProgress();
     }
 }
