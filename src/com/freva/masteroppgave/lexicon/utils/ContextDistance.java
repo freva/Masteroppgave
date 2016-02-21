c2084c9978c4296f8cb84596dedb3abcb85c13a5
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-02-21 21:15:58 +0100

    Added getters to ContextDistance

--- src/com/freva/masteroppgave/lexicon/utils/ContextDistance.java
+++ src/com/freva/masteroppgave/lexicon/utils/ContextDistance.java
@@ -1,12 +1,16 @@
 package com.freva.masteroppgave.lexicon.utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ContextDistance {
     Map<String, Map<String, List<Integer>>> distances = new HashMap<>();
+
+    public ContextDistance() {}
+
+    public ContextDistance(Map<String, Map<String, List<Integer>>> distances) {
+        this.distances = distances;
+    }
 
     /**
      * Stores distance between two tokens in an efficient manner. Allows for multiple distances.
@@ -22,6 +26,15 @@ public class ContextDistance {
         }
     }
 
+    public List<Integer> getDistance(String token1, String token2) {
+        if(token1.compareTo(token2) < 0) {
+            return distances.get(token1).get(token2);
+        } else {
+            List<Integer> dists = distances.get(token2).get(token1);
+            return dists.stream().map(i -> i*(-1)).collect(Collectors.toList());
+        }
+    }
+
 
     private List<Integer> getDistancesFromTo(String key1, String key2) {
         if(! distances.containsKey(key1)) {
@@ -34,5 +47,16 @@ public class ContextDistance {
         }
 
         return distancesFrom.get(key2);
+    }
+
+    public Set<Map.Entry<String, String>> getContextPairs() {
+        Set<Map.Entry<String, String>> pairs = new HashSet<>();
+        for(Map.Entry<String, Map<String, List<Integer>>> entry: distances.entrySet()) {
+            for(String key2: entry.getValue().keySet()) {
+                pairs.add(new AbstractMap.SimpleEntry<>(entry.getKey(), key2));
+            }
+        }
+
+        return pairs;
     }
 }
