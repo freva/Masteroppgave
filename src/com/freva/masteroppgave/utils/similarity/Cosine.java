af641c9603c8a4c1023be2be038c8f76323071de
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-02-24 18:47:32 +0100

    Created ArrayUtils with general array operations like sum and dotProduct

--- src/com/freva/masteroppgave/utils/similarity/Cosine.java
+++ src/com/freva/masteroppgave/utils/similarity/Cosine.java
@@ -1,5 +1,6 @@
 package com.freva.masteroppgave.utils.similarity;
 
+import com.freva.masteroppgave.utils.ArrayUtils;
 import com.freva.masteroppgave.utils.progressbar.Progressable;
 
 import java.util.*;
@@ -30,19 +31,21 @@ public class Cosine<T> implements Progressable {
      * @return Normalized co-occurrence array
      */
     private static double[][] normalize(int[][] values) {
-        int T = sumArray(values);
+        int T = ArrayUtils.sum(values);
         double[][] normalized = new double[values.length][values[0].length];
 
-        int[] columnSums = IntStream.range(0, values[0].length).map(i -> sumColumn(values, i)).toArray();
-        int[] rowSums = IntStream.range(0, values.length).map(i -> sumRow(values, i)).toArray();
+        int[] columnSums = IntStream.range(0, values[0].length).map(i -> ArrayUtils.sumColumn(values, i)).toArray();
+        int[] rowSums = IntStream.range(0, values.length).map(i -> ArrayUtils.sumRow(values, i)).toArray();
 
         for(int row = 0; row<values.length; row++) {
-            //normalized[row] = new double[row+1];
             for(int column = 0; column<values[0].length; column++) {
                 int over = T * values[row][column] - rowSums[row] * columnSums[column];
                 int under = rowSums[row]*(T-rowSums[row])*columnSums[column]*(T-columnSums[column]);
-                normalized[row][column] = over / Math.sqrt(under);
-                normalized[row][column] = normalized[row][column] > 0 ? Math.sqrt(normalized[row][column]) : 0;
+                double temp = over / Math.sqrt(under);
+
+                if(temp > 0) {
+                    normalized[row][column] = Math.sqrt(normalized[row][column]);
+                }
             }
         }
         return normalized;
@@ -51,13 +54,13 @@ public class Cosine<T> implements Progressable {
 
     private List<PairSimilarity<T>> calculateCosines(double[][] normalized, List<T> entries) {
         double[] rowSqSum = Arrays.stream(normalized)
-                .mapToDouble(row -> Math.sqrt(cross(row, row)))
+                .mapToDouble(row -> Math.sqrt(ArrayUtils.dotProduct(row, row)))
                 .toArray();
 
         List<PairSimilarity<T>> similarities = new ArrayList<>();
         for(int row = 0; row<normalized.length; row++) {
             for(int col = 0; col<row; col++, currentProgress++) {
-                double mul = cross(normalized[row], normalized[col]);
+                double mul = ArrayUtils.dotProduct(normalized[row], normalized[col]);
                 double cosine = mul / (rowSqSum[row] * rowSqSum[col]);
                 double similarity = Double.isNaN(cosine) ? 0 : cosine;
                 similarities.add(new PairSimilarity<>(entries.get(row), entries.get(col), similarity));
@@ -67,27 +70,6 @@ public class Cosine<T> implements Progressable {
         return similarities;
     }
 
-    private static double cross(double[] a, double[] b) {
-        double sum = 0;
-        for(int i=0; i<a.length; i++) {
-            if(a[i] == 0 || b[i] == 0) continue;
-            sum += a[i] * b[i];
-        }
-        return sum;
-    }
-
-
-    private static int sumArray(int[][] array) {
-        return IntStream.range(0, array.length).map(i -> sumColumn(array, i)).sum();
-    }
-
-    private static int sumColumn(int[][] array, int column) {
-        return IntStream.range(0, array.length).map(i -> array[i][column]).sum();
-    }
-
-    private static int sumRow(int[][] array, int row) {
-        return Arrays.stream(array[row]).sum();
-    }
 
     @Override
     public double getProgress() {
