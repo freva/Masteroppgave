d9c2befb24b6961889ac4ab248aa33628acb44d2
brageej <brageej@stud.ntnu.no>
2016-03-01 10:22:25 +0100

    emojiLexicon done

--- src/com/freva/masteroppgave/emoji/emojiDict.java
+++ src/com/freva/masteroppgave/emoji/emojiDict.java
@@ -1,9 +1,9 @@
 package com.freva.masteroppgave.emoji;
 
 
-import com.freva.masteroppgave.preprocessing.filters.Filters;
 import com.freva.masteroppgave.utils.FileUtils;
 import com.freva.masteroppgave.utils.JSONUtils;
+import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -13,13 +13,16 @@ import java.util.HashMap;
 
 public class emojiDict {
 
-    private static final File emojis = new File("res/data/emojis.json");
-    private static final File emojiDictionary2 = new File("res/data/emojiDictionary.json");
+    private static final File emojisFile = new File("res/data/emojis.json");
+    private static final File afinnFile = new File("res/data/afinn111.json");
+    private static final File emojiLexiconFile = new File("res/data/emojiLexicon.json");
 
     public static void main(String[] args) throws IOException {
-        String json = FileUtils.readEntireFileIntoString(emojis);
+        String afinnJson = FileUtils.readEntireFileIntoString(afinnFile);
+        HashMap<String, Double> afinnLexicon = JSONUtils.fromJSON(afinnJson, new TypeToken<HashMap<String, Double>>(){});
+        String json = FileUtils.readEntireFileIntoString(emojisFile);
         JSONArray jsonArray = new JSONArray(json);
-        HashMap<String, String[]> emojiDictionary = new HashMap<>();
+        HashMap<String, Double> emojiLexicon = new HashMap<>();
         for(int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
             JSONArray aliases = (JSONArray) jsonObject.get("aliases");
@@ -29,11 +32,21 @@ public class emojiDict {
                 tags[j] = jsonTags.getString(j);
             }
             String alias = aliases.getString(0);
-            if(tags.length > 0 && !tags[0].equals("flag")) emojiDictionary.put(alias, tags);
+            if(tags.length > 0 && !tags[0].equals("flag")){
+                double sentimentScore = 0;
+                System.out.println(tags[0] + " " + afinnLexicon.size());
+                for(String tag : tags) {
+                    if(afinnLexicon.containsKey(tag)) {
+                        sentimentScore += afinnLexicon.get(tag);
+                    }
+                }
+                sentimentScore = sentimentScore/tags.length;
+                if(sentimentScore != 0) {
+                    emojiLexicon.put(alias, sentimentScore/tags.length);
+                }
+            }
         }
-        String JSONNGrams = JSONUtils.toJSON(emojiDictionary, true);
-        FileUtils.writeToFile(emojiDictionary2, JSONNGrams);
-        System.out.println(Filters.replaceUnicodeEmoticons("An 😀awesome 😃string with a few 😉emojis!"));
-
+        String JSONNGrams = JSONUtils.toJSON(emojiLexicon, true);
+        FileUtils.writeToFile(emojiLexiconFile, JSONNGrams);
     }
 }
