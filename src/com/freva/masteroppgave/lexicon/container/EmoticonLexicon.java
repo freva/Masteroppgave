58db0b3fc95f1a2d1509976c09cc82a5392f2465
valerijf <valerijf@stud.ntnu.no>
2016-03-03 16:30:41 +0100

    Fixed bug that crashed when string was empty

--- src/com/freva/masteroppgave/lexicon/container/EmoticonLexicon.java
+++ src/com/freva/masteroppgave/lexicon/container/EmoticonLexicon.java
@@ -32,6 +32,6 @@ public class EmoticonLexicon {
                 replacementText.append(token);
             }
         }
-        return replacementText.toString().substring(1);
+        return replacementText.toString().trim();
     }
 }
