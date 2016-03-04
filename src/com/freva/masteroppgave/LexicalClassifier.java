e94948a7a3bee813b58dcd4cf0d2d4e255ca59fd
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-03-04 20:06:30 +0100

    Created optimal classification threshold finder

--- src/com/freva/masteroppgave/LexicalClassifier.java
+++ src/com/freva/masteroppgave/LexicalClassifier.java
@@ -2,6 +2,7 @@ package com.freva.masteroppgave;
 
 
 import com.freva.masteroppgave.classifier.Classifier;
+import com.freva.masteroppgave.classifier.Threshold;
 import com.freva.masteroppgave.lexicon.container.PriorPolarityLexicon;
 import com.freva.masteroppgave.preprocessing.filters.Filters;
 import com.freva.masteroppgave.preprocessing.preprocessors.DataSetEntry;
@@ -21,22 +22,32 @@ public class LexicalClassifier {
             Filters::removeInnerWordCharacters, Filters::removeNonAlphanumericalText, Filters::removeFreeDigits,
             Filters::removeRepeatedWhitespace, String::trim, String::toLowerCase);
 
+    private static final double neutralLowThreshold = -1.24;
+    private static final double neutralHighThreshold = 0.61;
+
 
     public static void main(String[] args) throws IOException {
         PriorPolarityLexicon priorPolarityLexicon = new PriorPolarityLexicon(Resources.OUR_LEXICON);
         TweetReader tweetReader = new TweetReader(Resources.SEMEVAL_2013_TEST);
         Classifier classifier = new Classifier(priorPolarityLexicon, filters);
-
+        Threshold threshold = new Threshold();
 
         ClassificationMetrics classificationMetrics = new ClassificationMetrics(DataSetEntry.Class.values());
         while (tweetReader.hasNext()) {
             DataSetEntry entry = tweetReader.readAndPreprocessNextDataSetEntry(3, 2);
 
-            DataSetEntry.Class predicted = classifier.classify(entry.getTweet());
+            double predictedSentiment = classifier.calculateSentiment(entry.getTweet());
+            DataSetEntry.Class predicted = DataSetEntry.Class.classifyFromThresholds(predictedSentiment, neutralLowThreshold, neutralHighThreshold);
+
             classificationMetrics.updateEvidence(entry.getClassification(), predicted);
+            threshold.updateEvidence(entry.getClassification(), predictedSentiment);
         }
 
         System.out.println(classificationMetrics.getClassificationReport());
         System.out.println(classificationMetrics.getNormalizedConfusionMatrixReport());
+        System.out.println("Current thresholds: [" + neutralLowThreshold + ", " + neutralHighThreshold +
+                "] with accuracy: " + classificationMetrics.getAccuracy());
+        System.out.println("Optimal thresholds: [" + String.format("%4.2f", threshold.getLowThreshold()) + ", " +
+                String.format("%4.2f", threshold.getHighThreshold()) + "] with accuracy: " + threshold.getMaxAccuracy());
     }
 }
---
+++ src/com/freva/masteroppgave/classifier/Threshold.java
@@ -0,0 +1,102 @@
+package com.freva.masteroppgave.classifier;
+
+import com.freva.masteroppgave.preprocessing.preprocessors.DataSetEntry;
+
+import java.util.*;
+
+public class Threshold {
+    private List<SentimentTuple> previousResults = new ArrayList<>();
+    private double lowThreshold;
+    private double highThreshold;
+    private double maxAccuracy;
+    private boolean cacheUpToDate = false;
+
+    public void updateEvidence(DataSetEntry.Class correctClass, double predictedSentiment) {
+        previousResults.add(new SentimentTuple(correctClass, predictedSentiment));
+        cacheUpToDate = false;
+    }
+
+
+    private void updateThresholds() {
+        Collections.sort(previousResults, Collections.reverseOrder());
+        int[][] counters = new int[DataSetEntry.Class.values().length][previousResults.size()];
+
+        counters[previousResults.get(0).correct.ordinal()][0]++;
+        for(int i=1; i<previousResults.size(); i++) {
+            for(int j=0; j<counters.length; j++) {
+                counters[j][i] = counters[j][i-1];
+            }
+            counters[previousResults.get(i).correct.ordinal()][i]++;
+        }
+
+        for(int i=1; i<previousResults.size()-2; i++) {
+            for (int j=i; j<previousResults.size()-1; j++) {
+                double tempAccuracy = getAccuracy(counters, i, j);
+
+                if(tempAccuracy > maxAccuracy) {
+                    maxAccuracy = tempAccuracy;
+                    lowThreshold = (previousResults.get(i-1).predicted + previousResults.get(i).predicted) / 2;
+                    highThreshold = (previousResults.get(j).predicted + previousResults.get(j+1).predicted) / 2;
+                }
+            }
+        }
+
+        cacheUpToDate = true;
+    }
+
+
+    /**
+     * Calculates optimal lower bound for neutral class. Optimizes based on accuracy.
+     * @return Lower bound threshold
+     */
+    public double getLowThreshold() {
+        if(! cacheUpToDate) updateThresholds();
+        return lowThreshold;
+    }
+
+    /**
+     * Calculates optimal higher bound for neutral class. Optimizes based on accuracy.
+     * @return Higher bound threshold
+     */
+    public double getHighThreshold() {
+        if(! cacheUpToDate) updateThresholds();
+        return highThreshold;
+    }
+
+    /**
+     * Calculates accuracy achieved if the optimal bounds were used.
+     * @return Accuracy measure, value within [0, 1]
+     */
+    public double getMaxAccuracy() {
+        return maxAccuracy;
+    }
+
+
+    private double getAccuracy(int[][] counters, int start, int end) {
+        int correctNegative = counters[DataSetEntry.Class.NEGATIVE.ordinal()][start-1];
+        int correctPositive = counters[DataSetEntry.Class.POSITIVE.ordinal()][previousResults.size()-1] - counters[DataSetEntry.Class.POSITIVE.ordinal()][end+1];
+        int correctNeutral = counters[DataSetEntry.Class.NEUTRAL.ordinal()][end] - counters[DataSetEntry.Class.NEUTRAL.ordinal()][start];
+
+        return (double) (correctNegative+correctNeutral+correctPositive) / previousResults.size();
+    }
+
+    private class SentimentTuple implements Comparable<SentimentTuple> {
+        private DataSetEntry.Class correct;
+        private double predicted;
+
+        SentimentTuple(DataSetEntry.Class correct, double predicted) {
+            this.correct = correct;
+            this.predicted = predicted;
+        }
+
+        public String toString() {
+            return "[" + correct.name() + ", " + predicted + "]";
+        }
+
+        @Override
+        public int compareTo(SentimentTuple o) {
+            double diff = o.predicted - predicted;
+            return diff != 0 ? (int) Math.signum(diff) : correct.compareTo(o.correct);
+        }
+    }
+}
