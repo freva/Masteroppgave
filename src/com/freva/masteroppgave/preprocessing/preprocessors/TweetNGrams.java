e72ec4ae984c8db583fb7483d234dc32a591b18c
brageej <brageej@stud.ntnu.no>
2016-03-10 19:39:48 +0100

    Added a new pruning method

--- src/com/freva/masteroppgave/preprocessing/preprocessors/TweetNGrams.java
+++ src/com/freva/masteroppgave/preprocessing/preprocessors/TweetNGrams.java
@@ -1,26 +1,36 @@
 package com.freva.masteroppgave.preprocessing.preprocessors;
 
+import com.freva.masteroppgave.preprocessing.filters.Filters;
 import com.freva.masteroppgave.preprocessing.filters.WordFilters;
+import com.freva.masteroppgave.preprocessing.reader.LineReader;
 import com.freva.masteroppgave.preprocessing.reader.TweetReader;
-import com.freva.masteroppgave.utils.tools.NGrams;
+import com.freva.masteroppgave.utils.Parallel;
 import com.freva.masteroppgave.utils.progressbar.Progressable;
+import com.freva.masteroppgave.utils.tools.NGrams;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
 
 public class TweetNGrams implements Progressable {
-    private TweetReader tweetReader;
+
+
+    private LineReader tweetReader;
 
     /**
      * Finds all frequent n-grams in a file, treating each new line as a new document.
      * @param input File with documents to generate n-grams for
      * @param n Maximum n-gram length
      * @param frequencyCutoff Smallest required frequency to include n-gram
-     * @param filters List of filters to apply to document before generating n-grams
+     * @param filters List of TWEET_FILTERS to apply to document before generating n-grams
      * @return Map of n-grams as key and number of occurrences as value
      * @throws IOException
      */
@@ -29,6 +39,7 @@ public class TweetNGrams implements Progressable {
         NGramTree tree = new NGramTree();
         Pattern containsAlphabet = Pattern.compile(".*[a-zA-Z]+.*");
         int lineCounter = 0;
+
 
         for(String line: tweetReader) {
             if(lineCounter % 50000 == 0 && lineCounter++ != 0) {
@@ -48,6 +59,30 @@ public class TweetNGrams implements Progressable {
         return tree.getNGrams((int) (frequencyCutoff*lineCounter));
     }
 
+    public final Map<String, Integer> getFrequentNGramsParallel(File input, int n, double frequencyCutoff, List<Function<String, String>> filters) throws IOException {
+        this.tweetReader = new LineReader(input);
+        NGramTree tree = new NGramTree();
+        Pattern containsAlphabet = Pattern.compile(".*[a-zA-Z]+.*");
+        final AtomicInteger lineCounter = new AtomicInteger(0);
+        Parallel.For(tweetReader, line ->{
+            synchronized (lineCounter){
+                if(lineCounter.incrementAndGet() % 50000 == 0) {
+                    tree.pruneInfrequent((int) (frequencyCutoff * lineCounter.intValue()) / 2);
+                }
+            }
+            line = Filters.chain(line, filters);
+            for(String[] nGramTokens: NGrams.getSyntacticalNGrams(line, n)) {
+                String nGram = StringUtils.join(nGramTokens, " ");
+                if(! containsAlphabet.matcher(nGram).find()) continue;
+                if(WordFilters.containsIntensifier(nGramTokens) || WordFilters.containsNegation(nGramTokens)) continue;
+                if(WordFilters.isStopWord(nGramTokens[nGramTokens.length - 1])) continue;
+
+                tree.incrementNGram(nGramTokens);
+            }
+        });
+        return tree.getNGrams((int) (frequencyCutoff*lineCounter.intValue()));
+    }
+
 
     @Override
     public double getProgress() {
@@ -56,9 +91,9 @@ public class TweetNGrams implements Progressable {
 
 
     private class NGramTree {
-        private Node root = new Node();
+        private Node root = new Node("");
 
-        private void incrementNGram(String[] nGram) {
+        private synchronized void incrementNGram(String[] nGram) {
             Node current = root;
             for(String word: nGram) {
                 if(! current.hasChild(word)) {
@@ -70,6 +105,45 @@ public class TweetNGrams implements Progressable {
             current.numOccurrences++;
         }
 
+        private Node getNode(String[] nGram) {
+            Node current = root;
+            for(String word: nGram) {
+                if(! current.hasChild(word)) {
+                    return null;
+                }
+
+                current = current.getChild(word);
+            }
+            return current;
+        }
+
+        private void pruneTree(Node startNode){
+            startNode.visited = true;
+            for(Node child : startNode.children.values()){
+                if(!child.visited){
+                    pruneTree(child);
+                }
+            }
+            if(startNode != root && !root.hasChild(startNode)) {
+                for (String child : startNode.children.keySet()) {
+                    Node childNode = startNode.children.get(child);
+                    for (String phrase : childNode.phrases.keySet()) {
+                        String combinedPhrase = startNode.phrase + " " + phrase;
+                        Node nodeToAdjust = getNode(combinedPhrase.split(" "));
+                        if(nodeToAdjust != null) {
+                            nodeToAdjust.subtractNumOccurrences(childNode.numOccurrences);
+                            startNode.phrases.put(combinedPhrase, childNode.getPhraseOccurrences(phrase));
+                        }
+                    }
+                }
+                Node currentNode = getNode(startNode.phrase.split(" "));
+                if(currentNode != null){
+                    currentNode.subtractNumOccurrences(startNode.numOccurrences);
+                    startNode.phrases.put(startNode.phrase, startNode.numOccurrences);
+                }
+            }
+        }
+
         private void pruneInfrequent(int limit) {
             root.pruneInfrequent(limit);
         }
@@ -77,6 +151,7 @@ public class TweetNGrams implements Progressable {
         private Map<String, Integer> getNGrams(int limit) {
             Map<String, Integer> nGrams = new HashMap<>();
             root.pruneInfrequent(limit);
+            pruneTree(root);
             root.addFrequentPhrases(nGrams, limit, "");
             return nGrams;
         }
@@ -86,17 +161,42 @@ public class TweetNGrams implements Progressable {
     private class Node {
         private Map<String, Node> children = new HashMap<>();
         private int numOccurrences;
+        private boolean visited;
+        private String phrase;
+        private HashMap<String, Integer> phrases = new HashMap<>();
+
+        public Node(String phrase){
+            this.phrase = phrase;
+        }
 
         public boolean hasChild(String value) {
             return children.containsKey(value);
         }
 
+        public boolean hasChild(Node node){
+            for(Node child : children.values()){
+                if(child == node){
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public void addChild(String value) {
-            children.put(value, new Node());
+            children.put(value, new Node(value));
         }
 
         public Node getChild(String value) {
             return children.get(value);
+        }
+
+        public int getPhraseOccurrences(String phrase){
+            return phrases.get(phrase);
+        }
+
+
+        public void subtractNumOccurrences(int toSubtract) {
+            numOccurrences -= toSubtract;
         }
 
         private void pruneInfrequent(int limit) {
@@ -110,6 +210,8 @@ public class TweetNGrams implements Progressable {
                 }
             }
         }
+
+
 
         private void addFrequentPhrases(Map<String, Integer> map, int limit, String prefix) {
             for(Map.Entry<String, Node> child: children.entrySet()) {
