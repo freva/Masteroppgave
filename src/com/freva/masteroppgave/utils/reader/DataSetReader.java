7f9fadb7e022ea3f76f9b02ef38dea7b44d49c69
Valerij Fredriksen <valerijf@stud.ntnu.no>
2016-04-17 14:21:17 +0200

    Moved DataSetEntry to DataSetReader

--- src/com/freva/masteroppgave/utils/reader/DataSetReader.java
+++ src/com/freva/masteroppgave/utils/reader/DataSetReader.java
@@ -1,13 +1,17 @@
 package com.freva.masteroppgave.utils.reader;
 
-import com.freva.masteroppgave.preprocessing.preprocessors.DataSetEntry;
+import com.freva.masteroppgave.preprocessing.filters.Filters;
 import com.freva.masteroppgave.utils.progressbar.Progressable;
+import com.freva.masteroppgave.utils.reader.DataSetReader.DataSetEntry;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 public class DataSetReader implements Iterator<DataSetEntry>, Iterable<DataSetEntry>, Progressable {
+    private static final Pattern tab_regex = Pattern.compile("\t");
+
     private final LineReader lineReader;
     private final int tweetIndex;
     private final int classIndex;
@@ -32,5 +36,70 @@ public class DataSetReader implements Iterator<DataSetEntry>, Iterable<DataSetEn
 
     public double getProgress() {
         return lineReader.getProgress();
+    }
+
+
+    public class DataSetEntry {
+        private final Classification classification;
+        private String tweet;
+
+        public DataSetEntry(String line, int tweetIndex, int classIndex) {
+            String[] values = tab_regex.split(line);
+            tweet = values[tweetIndex];
+            classification = Classification.parseClassificationFromString(values[classIndex]);
+        }
+
+
+        public String getTweet() {
+            return tweet;
+        }
+
+        public Classification getClassification() {
+            return classification;
+        }
+
+        public void applyFilters(Filters filters) {
+            tweet = filters.apply(tweet);
+        }
+    }
+
+
+    public enum Classification {
+        POSITIVE, NEUTRAL, NEGATIVE;
+
+        public static Classification parseClassificationFromString(String classification) {
+            switch (classification) {
+                case "positive":
+                    return Classification.POSITIVE;
+                case "neutral":
+                    return Classification.NEUTRAL;
+                case "negative":
+                    return Classification.NEGATIVE;
+                default:
+                    throw new IllegalArgumentException("Unknown class: " + classification);
+            }
+        }
+
+        public static Classification classifyFromThresholds(double value, double lowThresh, double highThresh) {
+            if (value < lowThresh) {
+                return NEGATIVE;
+            } else if (value > highThresh) {
+                return POSITIVE;
+            } else {
+                return NEUTRAL;
+            }
+        }
+
+        public boolean isPositive() {
+            return this == POSITIVE;
+        }
+
+        public boolean isNeutral() {
+            return this == NEUTRAL;
+        }
+
+        public boolean isNegative() {
+            return this == NEGATIVE;
+        }
     }
 }
